d0ecde500c55787005a1ff099e0da93ff154db11
Shchuenai <xd9955613@hotmail.com>
2017-10-18 18:59:16 +0800

    test

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+app.use(express.static('styles'));
+app.use(express.static(__dirname));
+
+var port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`listening on ${port}`);
+});
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
