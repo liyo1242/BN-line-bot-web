95cb5244bea349bd540c06176813441598de6ccb
liyo1242 <liyo1242@gmail.com>
2017-10-18 22:59:57 +0800

    start map

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@
         });
 
         // Create the search box and link it to the UI element.
-        var input = document.getElementById('pac-input');
+        var input = document.getElementById('on-the-place');
         var searchBox = new google.maps.places.SearchBox(input);
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
