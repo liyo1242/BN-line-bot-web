864ca7dbe77ccf438a4482b8e5eb97e5047cb1a6
liyo1242 <liyo1242@gmail.com>
2017-10-20 11:27:43 +0800

    jquery test

--- scripts/main.js
+++ scripts/main.js
@@ -9,12 +9,16 @@
 
         // Create the search box and link it to the UI element.
         var input = document.getElementById('on-the-place');
+        var input2 = document.getElementById('get-off-location');
         var searchBox = new google.maps.places.SearchBox(input);
+        var searchBox2 = new google.maps.places.SearchBox(input2);
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input2);
 
         // Bias the SearchBox results towards current map's viewport.
         map.addListener('bounds_changed', function() {
           searchBox.setBounds(map.getBounds());
+          searchBox2.setBounds(map.getBounds());
         });
 
         var markers = [];
@@ -22,6 +26,51 @@
         // more details for that place.
         searchBox.addListener('places_changed', function() {
           var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();  //鎖定範圍搜尋
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location  // 得到位置
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+         searchBox2.addListener('places_changed', function() {
+          var places = searchBox2.getPlaces();
 
           if (places.length == 0) {
             return;
