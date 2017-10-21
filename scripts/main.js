6d65522f2f9953f27d642530ec3bafed14998af7
liyo1242 <liyo1242@gmail.com>
2017-10-21 20:10:38 +0800

    map center location

--- scripts/main.js
+++ scripts/main.js
@@ -4,18 +4,92 @@
 
       function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          mapTypeControl: false,
+          // mapTypeControl: false,
+          // scaleControl:false,
+          disableDefaultUI : true,
           center: {lat: 25.0339640, lng: 121.5644720},
           zoom: 13,
-          gestureHandling:'cooperative'
+          gestureHandling:'cooperative',
+          styles: [
+  		  {
+    		featureType: "landscape",
+    		elementType: "labels",
+    		stylers: [	{ "visibility": "off" }	]
+  		  },
+          {
+            featureType: 'transit.line',
+            elementType: 'geometry ',
+            stylers: [{visibility: 'off'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.fill',
+            stylers: [{"color": "#BDD2D4"}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{"color": "#C5C3E2"}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'labels.icon',
+            stylers: [{visibility: 'off'}]
+          },
+          {
+            featureType: 'poi.business',
+            elementType: 'geometry',
+            stylers: [{"color": "#F2F2F2"}]
+          },
+          {
+            featureType: 'poi.government',
+            elementType: 'geometry',
+            stylers: [{"saturation": -100 }]
+          },
+          {
+            featureType: 'poi.place_of_worship',
+            elementType: 'geometry',
+            stylers: [{"saturation": -100 }]
+          },
+          {
+            featureType: 'poi.sports_complex',
+            elementType: 'geometry',
+            stylers: [{"saturation": -100 }]
+          },
+          {
+            featureType: 'poi.school',
+            elementType: 'geometry',
+            stylers: [{"color": "#F2F2F2" }]
+          },
+          {
+            featureType: 'poi.medical',
+            elementType: 'geometry',
+            stylers: [{"saturation": -100 }]
+          },
+          {
+            featureType: 'landscape.natural.landcover',
+            elementType: 'geometry.fill',
+            stylers: [{"color": "#F2F2F2" }]
+          },
+          {
+            featureType: 'landscape.man_made',
+            elementType: 'geometry.fill',
+            stylers: [{"color": "#F2F2F2" }]
+          },
+          {
+            featureType: 'landscape.man_made',
+            elementType: 'geometry.stroke',
+            stylers: [{"color": "#E6E6E6" }]
+          }
+          ]
         });
 
         var geocoder = new google.maps.Geocoder;
         var infowindow = new google.maps.InfoWindow;
-        
-        map.addListener("dragend", function(){
 
-        	var center = map.getCenter();
+        map.addListener("dragend", function(){   
+
+        	var center = map.getCenter();  // 這是經緯度=====================================
         	geocodeLatLng(geocoder, map, infowindow,center);
 		});
 
@@ -29,7 +103,7 @@
         geocoder.geocode({'location': latlng}, function(results, status) {
           if (status === 'OK') {
             if (results[0]) {
-            	window.alert(results[0].formatted_address);  // 這是地址
+            	 // window.alert(results[0].formatted_address);  // 這是地址==================
             } else {
               window.alert('No results found');
             }
@@ -47,9 +121,10 @@
         this.originPlaceId = null;
         this.destinationPlaceId = null;
         this.travelMode = 'DRIVING';
+        //================================================================================
         var originInput = document.getElementById('on-the-place');
         var destinationInput = document.getElementById('get-off-location');
-
+		//================================================================================
         this.directionsService = new google.maps.DirectionsService;
         this.directionsDisplay = new google.maps.DirectionsRenderer;
         this.directionsDisplay.setMap(map);
@@ -63,18 +138,8 @@
         this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
       }
 
-      // Sets a listener on a radio button to change the filter type on Places
-      // Autocomplete.
-      // AutocompleteDirectionsHandler.prototype.setupClickListener = function(id, mode) {
-      //   var radioButton = document.getElementById(id);
-      //   var me = this;
-      //   radioButton.addEventListener('click', function() {
-      //     me.travelMode = mode;
-      //     me.route();
-      //   });
-      // };
-
       AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
+        // 範圍限界
         var me = this;
         autocomplete.bindTo('bounds', this.map);
         autocomplete.addListener('place_changed', function() {
