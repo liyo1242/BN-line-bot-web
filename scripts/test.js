bf4cbbf8838ce62f6173a4b69e6d987386fe8ee6
Shchuenai <xd9955613@hotmail.com>
2017-11-05 13:24:00 +0800

    back

--- scripts/test.js
+++ scripts/test.js
@@ -1,38 +1,59 @@
-$(document).ready(function(){
-    $("#confirm_1").click(function(){
+$(document).ready(function () {
+    $("#next_getOn").click(function () {
         getOnPlace = $("#getOnPlace").val();
-        if(getOnPlace)
-        {
+        if (getOnPlace) {
             $('#getOnPlace').attr('readonly', true);
-            $(".confirm").hide();      
-            $("#confirm_2,#getOff").show();
+            $(".btn").hide();
+            $("#next_getOff,#getOff,#back").show();
             decideStep = 1;
-        }
-        else{
+        } else {
             alert("請輸入上車位置");
         }
     });
-    $("#confirm_2").click(function(){
+    $("#next_getOff").click(function () {
         getOffPlace = $("#getOffPlace").val();
-        if(getOffPlace){
+        if (getOffPlace) {
             $('#getOffPlace').attr('readonly', true);
-            $(".confirm").hide();      
-            $("#confirm_3,#date").show();
+            $(".btn").hide();
+            $("#taxi,#date,#back").show();
             decideStep = 2;
-        }
-        else{
+        } else {
             alert("請輸入下車位置");
         }
     });
-    $("#confirm_3").click(function(){
+    $("#taxi").click(function () {
         dateTime = $("#dateTime").val();
-        if(dateTime){
+        if (dateTime) {
             alert(getOnPlace + "\n" + getOffPlace + "\n" + dateTime);
-        }
-        else{
+        } else {
             alert("請輸入日期時間");
         }
     });
+    $("#back").click(function () {
+        switch (decideStep) {
+            case 0:
+                alert("error");
+                break;
+            case 1:
+                $("#getOnPlace").attr("readOnly", false).val("");
+                $("#getOffPlace").val("");
+                $(".btn,#getOff").hide();
+                $("#next_getOn").show();
+                getOnPlace = "";
+                decideStep = 0;
+                break;
+            case 2:
+                $("#getOffPlace").attr("readOnly", false).val("");
+                $("#dateTime").val("");
+                $(".btn,#date").hide();
+                $("#next_getOff,#back").show();
+                getOffPlace = "";
+                decideStep = 1;
+                break;
+            default:
+                alert("error");
+        }
+    })
 });
 
 var decideStep = 0;
