3ba82a399f4969bcf306e2d56da1144a1992ce59
Shchuenai <xd9955613@hotmail.com>
2017-11-05 12:39:18 +0800

    btn

--- scripts/test.js
+++ scripts/test.js
@@ -1,20 +1,41 @@
 $(document).ready(function(){
     $("#confirm_1").click(function(){
-        $(".confirm").hide();
-        $("#confirm_2,#getOff").show();
-        decideStep = 1;
+        getOnPlace = $("#getOnPlace").val();
+        if(getOnPlace)
+        {
+            $('#getOnPlace').attr('readonly', true);
+            $(".confirm").hide();      
+            $("#confirm_2,#getOff").show();
+            decideStep = 1;
+        }
+        else{
+            alert("請輸入上車位置");
+        }
     });
     $("#confirm_2").click(function(){
-        $(".confirm").hide();
-        $("#confirm_3,#date").show();
-        decideStep = 2;
+        getOffPlace = $("#getOffPlace").val();
+        if(getOffPlace){
+            $('#getOffPlace').attr('readonly', true);
+            $(".confirm").hide();      
+            $("#confirm_3,#date").show();
+            decideStep = 2;
+        }
+        else{
+            alert("請輸入下車位置");
+        }
     });
     $("#confirm_3").click(function(){
-        var getOnPlace = $("#getOnPlace").val();
-        var getOffPlace = $("#getOffPlace").val();
-        var dateTime = $("#dateTime").val();
-        alert(getOnPlace + "\n" + getOffPlace + "\n" + dateTime);
+        dateTime = $("#dateTime").val();
+        if(dateTime){
+            alert(getOnPlace + "\n" + getOffPlace + "\n" + dateTime);
+        }
+        else{
+            alert("請輸入日期時間");
+        }
     });
 });
 
 var decideStep = 0;
+var getOnPlace;
+var getOffPlace;
+var dateTime;
